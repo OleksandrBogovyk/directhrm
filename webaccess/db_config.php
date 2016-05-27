8dbb65b7f1bffbe25876235c8c4c51ec98dee1c0
Oleksandr Bogovyk <obogovyk@gmail.com>
2016-05-27 17:27:59 +0300

    Update db_config.php

--- webaccess/db_config.php
+++ webaccess/db_config.php
@@ -1,35 +1,9 @@
 <?php
-###########################################################
-/*
-Softuse HRMS
-Copyright(c)2014-2015 Softuse ltd. All rights Reserved.
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program. If not, see http://www.gnu.org/licenses/gpl-3.0.html.
-
-For further information visit:
-http://github.com/Softuse
-obogovyk@gmail.com
-
-Version:      1.09.12
-Last update:  26.03.2015
-*/
-###########################################################
 
 // Database login credentials //
 define('DB_HOST','localhost');
 define('DB_USER','root');
-define('DB_PASS','mysqlroot');
+define('DB_PASS','*****');
 define('DB_NAME','hrms');
 
 ?>
